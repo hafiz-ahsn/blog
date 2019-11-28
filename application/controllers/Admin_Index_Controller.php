469920e5b195a13c51c0da68766c6247a9c44ebe
ahsan <hafizgee07@gmail.com>
2019-11-28 17:25:23 +0500

    ahsan

---
+++ application/controllers/Admin_Index_Controller.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin_Index_Controller extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('admin_index_view');
+	}
+}
---
+++ application/views/admin_index_view.php
@@ -0,0 +1,8 @@
+<?php
+	include 'admin_common/admin_hfiles.php';
+	include 'admin_common/admin_navbar.php';
+	include 'admin_common/admin_sidebar.php';
+	include 'admin_common/admin_content.php';
+	include 'admin_common/admin_fbar.php';
+	include 'admin_common/admin_ffiles.php';
+?>
